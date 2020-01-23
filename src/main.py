1aa5bce387b9d62654e06fc5f068947bdbf5a894
marsplus <sixie.yu@vanderbilt.edu>
2020-01-22 20:23:45 -0600

    updates
    add comments

--- src/main.py
+++ src/main.py
@@ -9,45 +9,68 @@ import torch
 
 np.random.seed(1)
 
-
+# generate synthetic network structures, for simulation purposes
 n = 128
 G = nx.watts_strogatz_graph(n, 10, 0.2)
 adj = nx.adjacency_matrix(G).todense()
 
-alpha1, alpha2, alpha3 = 0.6, 0, 0.4
+# trade-off parameters
+alpha1, alpha2, alpha3 = 0.5, 0.2, 0.3
 
+# randomly choose a subset of nodes, whose 
+# induced subgraph becomes the subgraph we 
+# are interested in
 center = np.random.choice(range(G.order()))
 S = list(G.neighbors(center)) + [center]
+# S_prime = V \ S
 S_prime = list(set(G.nodes()) - set(S))
+# torch.LongTensor(): PyTorch way to say that S is some index set
 S = torch.LongTensor(S)
 S_prime = torch.LongTensor(S_prime)
 
-# haven't figured out how to take budget constraint into account
-budget = 1.5
+# those input parameters as in Algorithm 1 in the write-up
+budget = 2
 learning_rate = 1e-1
 Alpha = [alpha1, alpha2, alpha3]
 Attacker = Threat_Model(S, S_prime, Alpha, budget, learning_rate, G)
+
+# define an optimizer to solve our optimization problem
+# Attacker.parameters() tells the optimizer what we wanna optimize
 opt = torch.optim.SGD(Attacker.parameters(), lr=learning_rate)
 
+# stores the experimental results
 ret = {'lambda_1': [], 'lambda_1_S': [], 'centrality': []}
 Losses = []
+
+# implement Algorithm 1 in the write-up
 while True:
     Loss = Attacker()
     Losses.append(Loss.item())
     lambda1_S, lambda1, normalizedCut = Attacker.getRet()
+
     ret['lambda_1'].append(lambda1.item())
     ret['lambda_1_S'].append(lambda1_S.item())
     ret['centrality'].append(normalizedCut.item())
 
+    # opt.zero_grad(): clear accumulated gradients in the last step
+    # Loss.backward(): compute the gradient of the Loss w.r.t the optimizer (the adjacency matrix in our case)
     opt.zero_grad()
     Loss.backward()
 
+    # how much budget consumed in this step
     budget_this_step = Attacker.get_step_budget()
+
+    # how much budget has been used until now
     current_used_budget = Attacker.get_used_budget()
+
+    # whether the attacker's budget can afford it finish this step?
     if current_used_budget + budget_this_step <= budget:
+        # opt.step(): actually adds the perturbation (gradients x learning rate) to the adjacency matrix
         opt.step()
+        # attacker updates the amount of used budget
         Attacker.update_used_budget(budget_this_step)
     else:
         break
-                
+
+# for plotting purposes         
 ret = pd.DataFrame(ret)
--- src/threat_model.py
+++ src/threat_model.py
@@ -9,30 +9,36 @@ class Threat_Model(nn.Module):
         self.numNodes = len(G)
 
         self.S = S
-        # S_prime = V \ S
         self.S_prime = S_prime
         
         self.alpha_1, self.alpha_2, self.alpha_3 = Alpha
         self.budget = budget
         self.learning_rate = learning_rate
 
+        # tracks the amount of budget used
         self.used_budget = torch.zeros(1)
         
+        # those eigenvalues in the objective function
         self.lambda1_S_prime = 0
         self.lambda1_S = 0
         self.normalizedCut = 0
         self.lambda1 = 0
         
+        # the pristine adjacency matrix
         adj = nx.adjacency_matrix(G).todense()
         self.mask = torch.tensor(adj, dtype=torch.float32)
         
-        # eigenvals and eigenvectors associated with the largest eig-value of A (since mask = A)
+        # eigenvals and eigenvectors associated with the largest eig-value of adj (since mask = adj)
+        # the eigenvalues returned by torch.symeig() are in ascending order
+        # so the last one is the largest eigenvalue we want 
         eigVals, eigVecs = torch.symeig(self.mask, eigenvectors=True)
         self.eig_v = eigVecs[:, -1]
         self.lambda1_original = eigVals[-1]
        
-        # optimizer
+        # the thing we wanna optimize
+        # requires_grad_(True): tells PyTorch to starting tracking the gradients of this parameter
         self.adj_tensor = torch.tensor(adj, dtype=torch.float32).requires_grad_(True)
+        # tell PyTorch that self.adj_tensor is a parameter of Threat_Model, which we will optimize over
         self.adj_tensor = nn.Parameter(self.adj_tensor)
         
         # masking the gradients backpropagated to adj_tensor
@@ -51,19 +57,28 @@ class Threat_Model(nn.Module):
         x_s = torch.zeros(self.numNodes)
         x_s[self.S] = 1
     
-        # select the sub-matrix corresponding to S
+        # select the sub adjacency matrix corresponding to S and S_prime
         adj_tensor_S = torch.index_select(torch.index_select(self.adj_tensor, 0, self.S), 1, self.S)
         adj_tensor_S_prime = torch.index_select(torch.index_select(self.adj_tensor, 0, self.S_prime), 1, self.S_prime)
     
         # all sorts of largest eigenvalues 
+        # Note: need to set eigenvectors=True when calling torch.symeig, which is 
+        # required in order to do automatic differentiation
+        # eigValues, eigVecs = torch.symeig()
         self.lambda1 = torch.max(torch.symeig(self.adj_tensor, eigenvectors=True)[0])
         self.lambda1_S = torch.max(torch.symeig(adj_tensor_S, eigenvectors=True)[0])
         self.lambda1_S_prime = torch.max(torch.symeig(adj_tensor_S_prime, eigenvectors=True)[0])
         
         # centrality measure
-        self.normalizedCut = torch.mm(x_s.view(1, -1), torch.mm(L, x_s.view(-1, 1))) / (0.5 * adj_tensor_S.sum())
+        # torch.mm(): do matrix multiplication
+        # x_s.view(1, -1): convert x_s to a row vector
+        normalization_const = (0.5 * adj_tensor_S.sum())
+        self.normalizedCut = torch.mm(x_s.view(1, -1), torch.mm(L, x_s.view(-1, 1))) / normalization_const
         
-        # loss function 
+        # loss function (the negative of U_a)
+        # since we defined self.adj_tensor as a parameter, PyTorch automatically tracks 
+        # the functional relation between Loss and self.adj_tensor, and correspondingly computes derivatives
+        # of Loss w.r.t self.adj_tensor 
         Loss = -1 * (self.alpha_1 * self.lambda1_S - \
                      self.alpha_2 * self.lambda1_S_prime + self.alpha_3 * self.normalizedCut)
         
@@ -75,7 +90,9 @@ class Threat_Model(nn.Module):
     # budget consumed in each step
     def get_step_budget(self):
         if self.adj_tensor.grad != None:
+            # perturbation = gradient x learning rate
             pert = self.adj_tensor.grad * self.learning_rate
+            # budget used in this step is the operator norm of pert
             step_budget = torch.max(torch.abs(torch.symeig(pert)[0]))
             return step_budget
 
@@ -87,7 +104,6 @@ class Threat_Model(nn.Module):
     def get_used_budget(self):
         return self.used_budget.clone()
 
-    # return the results that we are interested in
     def getRet(self):
         return self.lambda1_S, self.lambda1, self.normalizedCut
 
