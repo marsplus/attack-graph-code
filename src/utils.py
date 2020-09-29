005a4c691dc42c9a3cdf40912149d775100df697
marsplus <sixie.yu@vanderbilt.edu>
2020-09-29 17:57:52 -0500

    updated auxiliary functions in utils.py

--- src/utils.py
+++ src/utils.py
@@ -3,7 +3,16 @@ import torch
 # an iterative method based on Rayleigh 
 # quotient to estimate the largest eigenvalue
 # and the associated eigenvector
-def power_method(mat, Iter=50):
+def power_method(mat, Iter=50, output_format='tensor'):
+    """
+        mat: input matrix, the default format is tensor in PyTorch.
+             Numpy array can also be handled
+        Iter: #iterations to estimate the leading eigenvector
+        output_format: set this to 'array' if the output should be in numpy array
+    """
+    if type(mat) != torch.Tensor:
+        mat = torch.tensor(mat, dtype=torch.float32)
+
     if mat.sum() == 0:
         return torch.zeros(len(mat))
 
@@ -19,10 +28,21 @@ def power_method(mat, Iter=50):
         if Diff <= flag:
             break
         x = x_new
-    return x_new
+    if output_format == 'tensor':
+        return x_new
+    elif output_format == 'array':
+        return x_new.numpy()
+    else:
+        raise ValueError("Unknown return type.")
 
 
-def estimate_sym_specNorm(mat, Iter=50):
+def estimate_sym_specNorm(mat, Iter=50, numExp=20):
+    """
+    mat: input matrix, the default format is tensor in PyTorch.
+         Numpy array can also be handled
+    Iter: #iterations to estimate the leading eigenvector
+    numExp: the #experiments to run in order to estimate the spectral norm
+    """
     if mat.sum() == 0:
         return 0
 
@@ -33,14 +53,13 @@ def estimate_sym_specNorm(mat, Iter=50):
 
     ## run the power method multiple times
     ## to make the estimation stable
-    numExp = 20
     spec_norm = 0
     for i in range(numExp):
         v = power_method(M, Iter)
         u = power_method(-M, Iter)
         spec_norm += torch.max( torch.abs(v @ M @ v), torch.abs(u @ (-M) @ u) )
     spec_norm /= numExp
-    return spec_norm
+    return spec_norm.item()
 
 
 def get_submatrix(mat, row_idx, col_idx):
